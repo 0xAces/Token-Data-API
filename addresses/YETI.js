c2e05a7d94846b1a7b77db7c4884fa3c9a7b9221
0xAces <acesyeti@gmail.com>
2022-04-23 18:54:56 -0700

    update YETi circulating

--- addresses/YETI.js
+++ addresses/YETI.js
@@ -1,29 +1,31 @@
 const avax_addresses = {
-    treasury: "0xed287c5cf0b7124c0c0d1de0db2ff48d61386e61",
-    investorsLockup: [
-      "0xdcef9e1d37a7079af00132e34a28b7840cb5a2be",
-      "0x215a3528ab51346d1625b66113208ad6b93a1305",
-      "0x7e70f95d862cd82c4f665566bcfc7149c0a77551",
-      "0x8e5285cf6327deecb87d4346a165a88e58ded249",
-      "0xdcb1fdeebee7d8ba20a8648a34c365c6d56971db"
-    ],
-    teamLockup: [
-      "0x8690587dbb5d2e52586a97e830d3c2de19bf2041",
-      "0x72cb70af132f7ae2301108036b068c4bad5ddb12",
-      "0xd3c6af7e1337e46c00586739d4c231ff70efeedf",
-      "0x9efd007e69454983f3744e82576c9d489f260ffe",
-      "0x57deea27f4b1e326ab7bc59ed43a80da1708fed4",
-      "0x91d7a8aadb37102db38e4ce195175a0ab4307cde",
-      "0x2b9dd40ec445e9368258d354ddcfe57607c823fa",
-      "0x78b3df2d477fff1fb5a643ef4dd8274df0bcce95",
-      "0x95f51eb39407a372cd1c26ff1ada4dcdb70db5a7",
-      "0xf7695ee425a3655a45530ed0fd5c63e9d9559ec0",
-      "0xe3a0f2ab3b0f282be8221a836f4ebbdfb9ef671e",
-    ],
-    YETIAddress: "0x77777777777d4554c39223c354a05825b2e8faa3",
-    teamAllocation: "0x5b7a9db4d69ad8f5bb7c443ffc3d35301851157e",
-    undistributedFarmRewards: "0xfffFffFFfFe8aA117FE603a37188E666aF110F39",
-    communityIssuance: "0x000000000bCD46cdD5a6E92C885b34CeAc6d2FF4",
-    pool2Rewarder: "0xf572455be31bf54cc7d0c6d69b60813171bb7b12"
-  }
-  module.exports = { avax_addresses }
+  treasury: "0xed287c5cf0b7124c0c0d1de0db2ff48d61386e61",
+  investorsLockup: [ 
+    "0xdcef9e1d37a7079af00132e34a28b7840cb5a2be",
+    "0x215a3528ab51346d1625b66113208ad6b93a1305",
+    "0x7e70f95d862cd82c4f665566bcfc7149c0a77551",
+    "0x8e5285cf6327deecb87d4346a165a88e58ded249",
+    "0xdcb1fdeebee7d8ba20a8648a34c365c6d56971db",
+    "0x2bEa4aA3959fa752D106d70b8CE5FDf6cCedf992"
+  ],
+  teamLockup: [
+    "0x8690587dbb5d2e52586a97e830d3c2de19bf2041",
+    "0x72cb70af132f7ae2301108036b068c4bad5ddb12",
+    "0xd3c6af7e1337e46c00586739d4c231ff70efeedf",
+    "0x9efd007e69454983f3744e82576c9d489f260ffe",
+    "0x57deea27f4b1e326ab7bc59ed43a80da1708fed4",
+    "0x91d7a8aadb37102db38e4ce195175a0ab4307cde",
+    "0x2b9dd40ec445e9368258d354ddcfe57607c823fa",
+    "0x78b3df2d477fff1fb5a643ef4dd8274df0bcce95",
+    "0x95f51eb39407a372cd1c26ff1ada4dcdb70db5a7",
+    "0xf7695ee425a3655a45530ed0fd5c63e9d9559ec0",
+    "0xe3a0f2ab3b0f282be8221a836f4ebbdfb9ef671e", 
+  ],
+  YETIAddress: "0x77777777777d4554c39223c354a05825b2e8faa3",
+  teamAllocation: "0x5b7a9db4d69ad8f5bb7c443ffc3d35301851157e",
+  YUSDCurveLPFarm: "0xfffFffFFfFe8aA117FE603a37188E666aF110F39",
+  communityIssuance: "0x000000000bCD46cdD5a6E92C885b34CeAc6d2FF4",
+  pool2Rewarder: "0x537533139e9240bB97031074ADf8E7f1a56E0833",
+  opsWallet: "0x718c79e7C12F7e2C1A5d641c81D02Eb7566dB9a8"
+}
+module.exports = { avax_addresses }
